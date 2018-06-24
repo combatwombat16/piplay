2ebc03ad590e37a70e786c8c5be6122452374895
combatwombat16 <combatwombat16@gmail.com>
2018-06-24 11:16:50 -0700

    Added hostname and better naming

--- logger/pullStats.py
+++ logger/pullStats.py
@@ -2,10 +2,14 @@ from getDateTimeInfo import get_datetime
 from getSenseHatStats import get_stats
 from getSystemInfo import get_sysinfo
 import json
+import socket
+
+def make_stat_dict(name, payload):
+	return {"host": socket.gethostname().split(".")[0], "type": name, "data": payload}
 
 stats = []
-stats.append({"host": "", "type": "datetime", "data": get_datetime()})
-stats.append({"host": "", "type": "sense", "data": get_stats()})
-stats.append({"host": "", "type": "system", "data": get_sysinfo()})
+stats.append(make_stat_dict("DateTime", get_datetime()))
+stats.append(make_stat_dict("Sense", get_stats()))
+stats.append(make_stat_dict("System", get_sysinfo()))
 
 print(json.dumps(stats))
