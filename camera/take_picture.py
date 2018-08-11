1f76164f6b295c32f1c8d6e0f81921b7fc7cb087
Ben Abrams <combatwombat16@gmail.com>
2018-08-11 10:46:42 -0700

    Beginning camera and video functionality.

---
+++ camera/take_picture.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python3
+
+from picamera import PiCamera
+from time import sleep
+from initialize import init_picture
+
+with PiCamera() as camera:
+	init_picture(camera)
+	camera.start_preview()
+	sleep(1)
+	camera.capture('./pictures/test_initialized.jpg')
+	
+	for i in camera.IMAGE_EFFECTS:
+		sleep(.25)
+		camera.image_effect = i
+		camera.capture('./pictures/test_ie_%s.jpg' % i)
+		sleep(.25)
+	init_picture(camera)
+
+	for i in camera.AWB_MODES:
+		sleep(.25)
+		camera.awb_mode = i
+		camera.capture('./pictures/test_awb_%s.jpg' % i)
+		sleep(.25)
+	init_picture(camera)
+
+	for i in camera.EXPOSURE_MODES:
+		sleep(.25)
+		camera.exposure_mode = i
+		camera.capture('./pictures/test_em_%s.jpg' % i)
+		sleep(.25)
+	init_picture(camera)
+	
+	camera.stop_preview()
---
+++ camera/take_video.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+
+from picamera import PiCamera
+from time import sleep
+from initialize import init_video
+
+with PiCamera() as camera:
+	init_video(camera)
+	camera.start_preview()
+	sleep(1)
+	camera.start_recording('./videos/test.h264')
+	sleep(10)
+	camera.stop_preview()
